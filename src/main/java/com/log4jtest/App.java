09a92a690b3d40635fa50035e253b39eba8a2de7
Kostegan <churilovkonstantin@gmail.com>
2017-01-28 02:40:18 +0400

    created files logfile.log, log4j.xml

--- src/main/java/com/log4jtest/App.java
+++ src/main/java/com/log4jtest/App.java
@@ -1,13 +1,18 @@
 package com.log4jtest;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger log = Logger.getLogger(App.class);
+    public static void main(String[] args) {
+        log.trace("trace message");
+        log.debug("debug message");
+        log.info("info message");
+        log.warn("warn message");
+        log.error("error message");
+        log.fatal("fatal message");
     }
 }
